407c6a3b9e1dc8ad5bf25b15fbbae9fc7a374020
aniketjha1729 <kumaraniket1729@gmail.com>
2021-10-24 15:37:51 +0530

    micro

---
+++ Employee-Rest-Producerr/src/main/java/com/tcs/restcontroller/EmployeeController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.tcs.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcs.bean.Employee;
+
+/**
+ * @author Aniket
+ *
+ */
+@RestController
+public class EmployeeController {
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+	public Employee firstPage() {
+
+		Employee emp = new Employee();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
+	}
+
+}
